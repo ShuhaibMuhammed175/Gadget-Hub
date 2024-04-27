4f986748a38dd566500c08bf8231f258e377a486
ShuhaibMuhammed175 <mshuhaib175@gmail.com>
2024-04-27 13:47:41 +0530

    Changes made

--- project/settings.py
+++ project/settings.py
@@ -13,6 +13,10 @@ import os
 from pathlib import Path
 import dj_database_url
 import environ
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 env = environ.Env()
 environ.Env.read_env()
 
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
